bc5507c8cb85a976a7ee9285540618d537b8de76
kandersen753 <kandersen@nevada.unr.edu>
2024-10-09 11:50:16 -0700

    Day 22 added

---
+++ 30_Days_of_Code/Day_22_Binary_Search_Trees.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cstddef>
+
+using namespace std;	
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+		int getHeight(Node* root){
+            //am i bottom?
+            if (root == NULL)
+            {
+                return -1;
+            }
+            //get left height
+            int lHeight = getHeight(root->left) + 1;
+            //get right height
+            int rHeight = getHeight(root->right) + 1;
+            
+            //return bigger
+            return std::max(lHeight, rHeight);     
+        }
+
+}; //End of Solution
+
+int main() {
+    Solution myTree;
+    Node* root = NULL;
+    int t;
+    int data;
+
+    cin >> t;
+
+    while(t-- > 0){
+        cin >> data;
+        root = myTree.insert(root, data);
+    }
+    int height = myTree.getHeight(root);
+    cout << height;
+
+    return 0;
+}
+    
+       
+            
+        
